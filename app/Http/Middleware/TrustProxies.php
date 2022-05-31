569cbccbd81f6392a1fe5b36eb744cedd730b597
momoko-fukuda <momo.taka0830@gmail.com>
2022-05-31 13:17:55 +0000

    05 create login system

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,8 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies;
+    // https使用設定 
+    protected $proxies='**';
 
     /**
      * The headers that should be used to detect proxies.
